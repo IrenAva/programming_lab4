12c7a45c3a9f91093fa792b6f0c0d66d5e3f592b
IrenAva <156667007+IrenAva@users.noreply.github.com>
2025-10-28 17:24:47 +0700

    Update array_function.c

--- src/array_function.c
+++ src/array_function.c
@@ -1,8 +1,43 @@
 #include "../include/array_function.h"
 
-void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, int *dest) {}
+void join_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
+    for (int i = 0; i < size_src1; i++) {
+        dest[i] = src1[i];
+    }
+    
+    for (int i = 0; i < size_src2; i++) {
+        dest[size_src1 + i] = src2[i];
+    }
+}
 
-void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {}
+void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
+    join_int_arrays(src1, size_src1, src2, size_src2, dest);
+    
+    int total = size_src1 + size_src2;
+    for (int i = 0; i < total - 1; i++) {
+        for (int j = 0; j < total - i - 1; j++) {
+            if (dest[j] > dest[j + 1]) {
+                // Меняем местами
+                int temp = dest[j];
+                dest[j] = dest[j + 1];
+                dest[j + 1] = temp;
+            }
+        }
+    }
+}
 
-void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {}
-
+void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {
+    if (size == 0) return;
+    
+    *min = src[0];
+    *max = src[0];
+    
+    for (int i = 1; i < size; i++) {
+        if (src[i] < *min) {
+            *min = src[i];
+        }
+        if (src[i] > *max) {
+            *max = src[i];
+        }
+    }
+}
