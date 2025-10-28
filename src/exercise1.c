88d2f1f612551fbb61d5bdb7eee0d47103b36b60
IrenAva <156667007+IrenAva@users.noreply.github.com>
2025-10-28 14:20:13 +0700

    Update exercise1.c

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
+int main() {
+    float numbers[10];
+    float sum = 0;
+    
+    for (int i = 0; i < 10; i++) {
+        scanf("%f", &numbers[i]);
+        sum += numbers[i];
+    }
+    
+    float average = sum / 10;
+    
+    printf("%.2f\n", average);
+    
     return 0;
 }
