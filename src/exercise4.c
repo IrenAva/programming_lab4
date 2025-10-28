901dab6962650de076adadfe206c6ca74a953e28
IrenAva <156667007+IrenAva@users.noreply.github.com>
2025-10-28 14:21:13 +0700

    Update exercise4.c

--- src/exercise4.c
+++ src/exercise4.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
+int main(int argc, char *argv[]) {
+    float numbers[10];
+    
+    for (int i = 0; i < 10; i++) {
+        scanf("%f", &numbers[i]);
+    }
+    
+    float X = atof(argv[1]);
+    int K = atoi(argv[2]);
+    
+    for (int i = 9; i > K; i--) {
+        numbers[i] = numbers[i - 1];
+    }
+    
+    numbers[K] = X;
+    
+    for (int i = 0; i < 10; i++) {
+        printf("%g ", numbers[i]);
+    }
+    printf("\n");
+    
     return 0;
 }
