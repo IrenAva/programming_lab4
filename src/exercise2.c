79cfa53f4051ac9d4f22edb141f8e5b5a0ede654
IrenAva <156667007+IrenAva@users.noreply.github.com>
2025-10-28 14:20:39 +0700

    Update exercise2.c

--- src/exercise2.c
+++ src/exercise2.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
+int main() {
+    float numbers[10];
+    
+    for (int i = 0; i < 10; i++) {
+        scanf("%f", &numbers[i]);
+    }
+    
+    for (int i = 9; i >= 0; i--) {
+        printf("%g ", numbers[i]);
+    }
+    printf("\n");
+    
     return 0;
 }
